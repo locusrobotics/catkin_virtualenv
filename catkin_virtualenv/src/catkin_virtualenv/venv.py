5ac04e18638cd8c0ab256f215791368aad22fa45
Gary Servin <gservin@locusrobotics.com>
2026-02-11 18:44:40 -0300

    Fix flake issues

--- catkin_virtualenv/src/catkin_virtualenv/venv.py
+++ catkin_virtualenv/src/catkin_virtualenv/venv.py
@@ -25,7 +25,6 @@ import os
 import re
 import shutil
 import subprocess
-import tempfile
 
 try:
     from urllib.request import urlretrieve
@@ -66,8 +65,9 @@ class Virtualenv:
             raise RuntimeError(error_msg)
 
         preinstall = [
-            "pip==24.0",
-            "pip-tools==7.4.1",
+            "pip==26.0.1",
+            "pip-tools==7.5.3",
+            "setuptools==82.0.0",
         ]
 
         builtin_venv = self._check_module(system_python, "venv")
